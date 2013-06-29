e73473fa974ee800a782b646ab002d1bb5b0317c
codesliced <codesliced@gmail.com>
2013-06-29 18:06:03 -0500

    testing carrierwave production env

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -41,13 +41,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   version :thumb do
     process :resize_to_fill => [200,200]
   end
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
